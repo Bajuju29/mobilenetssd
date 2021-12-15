87294c8332928493cc1f180f0ae317e03cbf08f8
Bajuju29 <89563731+Bajuju29@users.noreply.github.com>
2021-12-15 14:41:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,6 @@ def event_handle(event,json_line):
         elif msg == "แจมินน่ารักมาก" :
             replyObj = TextSendMessage(text="จบิงมาก")
             line_bot_api.reply_message(rtoken, replyObj)
-        
         elif msg == "covid" :
             url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
             response = requests.get(url)
